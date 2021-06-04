924b89c541597ece8f082cb5835cd6d65c83f888
Anderson R Damasceno <anderdam@gmail.com>
2021-06-03 23:40:19 -0300

    Angular, Interpolaçao, Diretiva e Binding

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,20 +8,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  private _listFilter: string = '';
 
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  public widthImg: number = 100;
+  public marginImg: number = 2;
+  public showImg: boolean = true;
+
+
+  public get listFilter(): string
+  {
+    return this._listFilter;
+  }
+
+  public set listFilter(value: string)
+  {
+    this._listFilter = value;
+    this.eventosFiltrados = this.listFilter ? this.eventFilter(this.listFilter) : this.eventos;
+  }
+
+  public eventFilter(searchBy: string): any
+  {
+    searchBy = searchBy.toLowerCase();
+    return this.eventos.filter((evento: { tema: string; localName: string }) =>
+      evento.tema.toLocaleLowerCase().indexOf(searchBy) !== -1 ||
+      evento.localName.toLocaleLowerCase().indexOf(searchBy) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void
   {
     this.getEventos();
-   }
+  }
+
+  btnShowImgChange()
+  {
+    this.showImg = !this.showImg;
+  }
+
+
 
   public getEventos():void
   {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
       error => console.log(error));
   }
 
--- README.md
+++ README.md
@@ -4,35 +4,33 @@ ProEventos - Curso da Udemy
 
 Repositório para a realização do projeto desenvolvido nos estudos do Curso "Seja Full-Stack com Asp.NET Core, Angular + EF Core 2021".
 
-**Projeto em progresso**
+## Projeto em progresso
 
 ## Tecnologias
 
 As seguintes ferramentas foram usadas na construção do projeto:
 
-**FrontEnd** 
+## FrontEnd
 
- - Angular 12
- - Bootstrap 4
- - TypeScript
- - SCSS
+- Angular 12
+- Bootstrap 4
+- TypeScript
+- SCSS
 
- **BackEnd**
+## BackEnd
 
 - C# 9
 - .Net 5
 - EF Core
 
-**Database**
+## Database
 
 - Sqlite
-
 
 ## Funcionalidades no projeto
 
 - [x] Cadastro de eventos
 - [x] Cadastro de palestrantes
-
 
 ## Autor
 
