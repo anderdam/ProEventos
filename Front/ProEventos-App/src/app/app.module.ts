23d5f62213093756aff5b5d2fd701e05011bc2d2
Anderson R Damasceno <anderdam@gmail.com>
2021-06-08 14:40:09 -0300

    Atualização de pacotes npm no projeto

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -1,33 +1,45 @@
-import { NgModule } from '@angular/core';
+//Angular
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
-import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
+import { NgModule } from '@angular/core';
+//NGX-BOOTSTRAP
 import { CollapseModule } from 'ngx-bootstrap/collapse';
-import { NavComponent } from './nav/nav.component';
-
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+//Components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
+import { NavComponent } from './nav/nav.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+//Services
+import { EventoService } from './services/evento.service';
+//PIPE
+import { DateTimeFormatPipe } from './helpers/DateTimeFormat.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventosComponent,
       PalestrantesComponent,
-      NavComponent
+      NavComponent,
+      DateTimeFormatPipe
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    CollapseModule.forRoot(),
     FormsModule,
+    TooltipModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    CollapseModule.forRoot(),
+    ModalModule.forRoot(),
   ],
-  providers: [],
+  providers: [EventoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,34 +1,38 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { Evento } from '../models/Evento';
+import { EventoService } from '../services/evento.service';
 
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
   styleUrls: ['./eventos.component.scss']
 })
-export class EventosComponent implements OnInit {
+export class EventosComponent implements OnInit
+{
+  modalRef!: BsModalRef;
 
-  private _listFilter: string = '';
+  private filtroListado = '';
 
-  public eventos: any = [];
-  public eventosFiltrados: any = [];
-  public widthImg: number = 100;
-  public marginImg: number = 2;
-  public showImg: boolean = true;
+  public eventos: Evento[] = [];
+  public eventosFiltrados: Evento[] = [];
+  public widthImg = 100;
+  public marginImg = 2;
+  public showImg = true;
 
 
   public get listFilter(): string
   {
-    return this._listFilter;
+    return this.filtroListado;
   }
 
   public set listFilter(value: string)
   {
-    this._listFilter = value;
+    this.filtroListado = value;
     this.eventosFiltrados = this.listFilter ? this.eventFilter(this.listFilter) : this.eventos;
   }
 
-  public eventFilter(searchBy: string): any
+  public eventFilter(searchBy: string): Evento[]
   {
     searchBy = searchBy.toLowerCase();
     return this.eventos.filter((evento: { tema: string; localName: string }) =>
@@ -37,28 +41,37 @@ export class EventosComponent implements OnInit {
     );
   }
 
-  constructor(private http: HttpClient) { }
+  constructor(private eventoService: EventoService, private modalService: BsModalService) { }
 
-  ngOnInit(): void
+  public ngOnInit(): void
   {
     this.getEventos();
   }
 
-  btnShowImgChange()
+  public btnShowImgChange(): void
   {
     this.showImg = !this.showImg;
   }
 
-
-
-  public getEventos():void
+  public getEventos(): void
   {
-    this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => {
-        this.eventos = response;
+    this.eventoService.getEventos().subscribe(
+      (eventos: Evento[]) => {
+        this.eventos = eventos;
         this.eventosFiltrados = this.eventos;
       },
       error => console.log(error));
   }
 
+  openModal(template: TemplateRef<unknown>): void {
+    this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
+  }
+
+  confirm(): void {
+    this.modalRef.hide();
+  }
+
+  decline(): void {
+    this.modalRef.hide();
+  }
 }
