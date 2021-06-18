d92121c0faa3aaee679d4fa93b8bbf10c11ceaa4
Anderson R Damasceno <anderdam@gmail.com>
2021-06-18 02:04:59 -0300

    Registration form

--- Front/ProEventos-App/src/app/componentes/user/registration/registration.component.ts
+++ Front/ProEventos-App/src/app/componentes/user/registration/registration.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { ValidatorField } from '@app/helpers/validatorField';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
+
+  get f(): any {
+    return this.form.controls;
+  }
+
+  constructor(public fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.validation();
+  }
+  private validation():void {
+
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('senha', 'confirmeSenha')
+    };
+
+    this.form = this.fb.group({
+      primeiroNome: ['', Validators.required],
+      ultimoNome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      userName: ['', Validators.required],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+      confirmeSenha: ['', Validators.required]
+    }, formOptions);
   }
 
 }
